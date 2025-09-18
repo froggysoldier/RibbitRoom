a7ced8c995237fd7e2e4ad54fb7e69e8afe99b7e
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 12:28:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get("*", (req, res) => {
 
 // Route: Nachrichten holen
 app.get('/messages', async (req, res) => {
-    const msgs = await Message.find().sort({ createdAt: -1 }).limit(20);
+    const msgs = await Message.find().sort({ createdAt: -1 })/*.limit(20)*/;
     res.json(msgs);
 });
 
@@ -90,4 +90,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server läuft auf Port ${PORT}`);
 });
+
 
