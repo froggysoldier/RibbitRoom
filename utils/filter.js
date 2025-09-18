5fdb42e461df3d7860fdfea10b2ba57eb000fd10
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 17:00:25 +0200

    Create filter.js

---
+++ utils/filter.js
@@ -0,0 +1,20 @@
+// filter.js
+const wordReplacements = {
+  "hallo": "ribbit",
+  "test": "****",
+  // hier weitere Wörter hinzufügen
+};
+
+function filterMessage(text) {
+  if (!text) return text;
+
+  Object.keys(wordReplacements).forEach(word => {
+    const replacement = wordReplacements[word];
+    const regex = new RegExp(`\\b${word}\\b`, "gi");
+    text = text.replace(regex, replacement);
+  });
+
+  return text;
+}
+
+module.exports = filterMessage;
