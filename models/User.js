ba8329a20a2762c700c80190088a85673160f0c0
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 18:50:51 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
+  email: { type: String, required: true, unique: true },
+  role: { type: String, default: "user" } // optional, falls du Rollen brauchst
 }, { timestamps: true });
 
 // Passwort hashen vor dem Speichern
