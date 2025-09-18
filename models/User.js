e4fdae1daa3f256f04dd55305aec103f3a4ac5bb
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 18:42:11 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user"
-  }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true }
 }, { timestamps: true });
+
+// Passwort hashen vor dem Speichern
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Passwort prüfen
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
