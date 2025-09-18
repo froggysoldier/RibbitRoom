18eb5948b587da59e006b0bd9494cf3144ccdfd4
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 16:21:38 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,24 @@
+// models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  username: {
+    type: String,
+    required: true,
+    unique: true, // Benutzername einzigartig
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // E-Mail einzigartig
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
