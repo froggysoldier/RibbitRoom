03f45531dbe110890baec2f88f8f8c393389cd47
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-18 17:44:30 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,23 @@
-// models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" }, // Admin-Role
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
