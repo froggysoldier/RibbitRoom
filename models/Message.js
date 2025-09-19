82f171757a0c87a26279710f0e01efd9a40c56e6
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-19 23:00:58 +0200

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  sender: String,
-  content: String,
+  sender: {
+    username: { type: String, required: true },
+    role: { type: String, default: "user" } // "user" oder "admin"
+  },
+  content: { type: String, required: true },
+  type: { type: String, default: "user" }, // "user" oder "system"
   createdAt: { type: Date, default: Date.now }
 });
 
