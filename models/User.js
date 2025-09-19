128e9f4085bc7161f238d8b92bbc1c5ed52cbb67
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-19 20:48:55 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,35 +1,21 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  role: { type: String, default: "user" }
-}, { timestamps: true });
-
-// Passwort hashen vor dem Speichern
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  try {
-    // Wenn das Passwort bereits ein Hash ist, nicht erneut hashen
-    if (this.password.startsWith("$2")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  role: { type: String, default: "user" } // user oder admin
 });
 
-// Passwort prüfen (defensiv)
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  if (!candidatePassword || !this.password) return false;
-  try {
-    return await bcrypt.compare(candidatePassword, this.password);
-  } catch {
-    return false;
-  }
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidate) {
+  return bcrypt.compare(candidate, this.password);
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
