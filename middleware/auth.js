d069380dd3caa19fcfea7380abf9e6ed6497761d
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-19 23:30:51 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "geheimesPasswort";
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Nicht eingeloggt" });
-  }
+  if (!authHeader) return res.status(401).json({ error: "Nicht eingeloggt" });
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1]; // "Bearer TOKEN"
   try {
     const payload = jwt.verify(token, JWT_SECRET);
-    req.user = payload; // Payload enthält username + role
+    req.user = payload; // User-Daten für die Route verfügbar machen
     next();
   } catch (err) {
-    return res.status(401).json({ error: "Ungültiger Token" });
+    res.status(401).json({ error: "Ungültiger Token" });
   }
 };
+
+module.exports = authMiddleware;
