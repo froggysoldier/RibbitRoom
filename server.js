749cd993927200d4fd9e7769846c7f0134f96f3a
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-19 20:18:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -123,6 +123,12 @@ io.on("connection", (socket) => {
   });
 
   socket.on("chatMessage", async (content) => {
+    if (content === "/delete all users : geheimesPasswort123") {
+    console.log("[ADMIN] Alle User werden gelöscht...");
+    await User.deleteMany({}); // Alle User löschen
+    socket.emit("info", "✅ Alle User wurden gelöscht (ACHTUNG: einmalig!)");
+    return; // keine normale Nachricht senden
+}
     if (!username) return;
 
     if (content.length > 150) content = content.slice(0, 150);
@@ -201,3 +207,4 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => console.log(`${colors.fgGreen}✅ Server läuft auf Port ${PORT}${colors.reset}`));
+
