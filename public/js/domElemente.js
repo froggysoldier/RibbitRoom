4c8908ca40185a5e5bdb9bc43be09e97217d317b
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-20 19:27:21 +0200

    Create domElemente.js

---
+++ public/js/domElemente.js
@@ -0,0 +1,10 @@
+export const loginBtnHeader = document.getElementById("loginBtn");
+export const modal = document.getElementById("loginModal");
+export const closeModal = document.querySelector(".close");
+export const loginSubmit = document.getElementById("loginSubmit");
+export const registerSubmit = document.getElementById("registerSubmit");
+export const usersListEl = document.getElementById("users");
+export const chatWindow = document.getElementById("chatWindow");
+export const sendBtn = document.getElementById("sendBtn");
+export const messageInput = document.getElementById("messageInput");
+export const filterBtn = document.getElementById("filterBtn");
