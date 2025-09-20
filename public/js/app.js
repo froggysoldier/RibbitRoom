7ce245ae8e95eeb59bebe77531f9d1a195c3d779
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-20 23:39:14 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -19,12 +19,10 @@ const state = {
   filterBtn: DOM.filterBtn
 };
 
-// Init
+// Socket, Auth & Chat initialisieren
 initSocket(state);
 initAuthHandlers(state);
 initChatHandlers(state);
 
-// Alte Nachrichten direkt laden, falls Token vorhanden
-if (state.token) {
-  loadMessages(state);
-}
+// Alte Nachrichten laden, falls Token vorhanden
+if (state.token) loadMessages(state);
