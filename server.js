07dfb825a82e606193927ade3a9641427de7e162
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-23 20:01:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const authRoutes = require("./routes/authRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const initSockets = require("./sockets/initSockets");
 
-const lastMessageTime = new Map();
-const lastSpamWarnTime = new Map();
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
@@ -24,15 +21,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
+// MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB verbunden"))
   .catch(err => console.error("❌ MongoDB Fehler:", err));
 
-// init sockets (registers socket handlers)
+// Socket-Events
 initSockets(io);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => console.log(`✅ Server läuft auf Port ${PORT}`));
-
-
-
