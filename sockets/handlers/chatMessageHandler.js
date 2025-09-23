a5fbdb8cb244a168c6f2c6ffe58eadff263f3c6f
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-23 12:16:55 +0200

    Update chatMessageHandler.js

--- sockets/handlers/chatMessageHandler.js
+++ sockets/handlers/chatMessageHandler.js
@@ -100,7 +100,7 @@ module.exports = function(socket, ctx) {
         io.emit("deletedMessages", []); // clients emptyen chat
         // notify all authenticated clients to reload messages (but not logout)
         for (const sid of authenticatedSockets) {
-          io.to(sid).emit("systemMessage", { text: "⚠️ Alle Nachrichten werden gelöscht!", type: "error" });
+          io.to(sid).emit("systemMessage", { text: "⚠️ Alle Nachrichten werden in kürze gelöscht!", type: "error" });
           setTimeout(() => {
             io.to(sid).emit("updateUsersAndMessages");
           }, 1500);
