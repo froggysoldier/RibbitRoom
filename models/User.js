9c6824e947d441d1208894fe17ddf8ceb6bc69ce
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 08:27:35 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,10 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" } // user oder admin
+  role: { type: String, default: "user" }, // user oder admin
+
+  verificationCode: String,
+  codeExpiresAt: Date
 });
 
 UserSchema.pre("save", async function(next) {
