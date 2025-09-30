c1a170bb17d91232d579c772eba86da384976400
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 08:51:47 +0200

    Update domElements.js

--- public/js/domElements.js
+++ public/js/domElements.js
@@ -9,7 +9,8 @@ export const chatWindow = document.getElementById("chatWindow");
 export const sendBtn = document.getElementById("sendBtn");
 export const messageInput = document.getElementById("messageInput");
 export const filterBtn = document.getElementById("filterBtn");
-export const codeBlock = document.getElementById("codeBlock");
+
+// Code-Modal
+export const codeModal = document.getElementById("codeBlock");
 export const codeSubmit = document.getElementById("codeSubmit");
 export const codeInput = document.getElementById("code");
-
