5a19c1e67fea53235742a7644f97f9e9a862f07f
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 10:18:18 +0200

    Update domElements.js

--- public/js/domElements.js
+++ public/js/domElements.js
@@ -1,16 +1,15 @@
-// public/js/domElements.js
-export const loginBtn = document.getElementById("loginBtn");
-export const modal = document.getElementById("loginModal");
-export const closeModal = document.querySelector(".close");
-export const loginSubmit = document.getElementById("loginSubmit");
-export const registerSubmit = document.getElementById("registerSubmit");
-export const usersListEl = document.getElementById("users");
-export const chatWindow = document.getElementById("chatWindow");
-export const sendBtn = document.getElementById("sendBtn");
-export const messageInput = document.getElementById("messageInput");
-export const filterBtn = document.getElementById("filterBtn");
+// Funktionen statt direkten DOM-Abfragen, damit die Elemente sicher existieren
+export const getLoginBtn = () => document.getElementById("loginBtn");
+export const getModal = () => document.getElementById("loginModal");
+export const getCloseModal = () => document.querySelector(".close");
+export const getLoginSubmit = () => document.getElementById("loginSubmit");
+export const getRegisterSubmit = () => document.getElementById("registerSubmit");
+export const getUsersListEl = () => document.getElementById("users");
+export const getChatWindow = () => document.getElementById("chatWindow");
+export const getSendBtn = () => document.getElementById("sendBtn");
+export const getMessageInput = () => document.getElementById("messageInput");
+export const getFilterBtn = () => document.getElementById("filterBtn");
 
-export const codeModal = document.getElementById("codeModal");
-export const codeInput = document.getElementById("code");
-export const codeSubmit = document.getElementById("codeSubmit");
-
+export const getCodeModal = () => document.getElementById("codeModal");
+export const getCodeInput = () => document.getElementById("code");
+export const getCodeSubmit = () => document.getElementById("codeSubmit");
