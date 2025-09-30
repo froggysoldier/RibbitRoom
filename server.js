f90c148f32e72eea0a3d677ff9bad6401d0dbb07
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 10:55:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import nodemailer from "nodemailer";
 import authRoutes from "./routes/authRoutes.js"; // ES Module import
 import path from "path";
 import { fileURLToPath } from "url";
@@ -23,36 +22,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB verbunden"))
 .catch(err => console.error("MongoDB Fehler:", err));
 
-
-async function testMail() {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === "true",
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-      }
-    });
-
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_FROM,
-      to: "ribbitroomrender@gmail.com", // test-eMail an dich selbst
-      subject: "Test-Mail von RibbitRoom",
-      text: "Hallo, das ist ein Test!",
-      html: "<b>Hallo, das ist ein Test!</b>"
-    });
-
-    console.log("Mail gesendet:", info.messageId);
-  } catch (err) {
-    console.error("Mail-Test fehlgeschlagen:", err);
-  }
-}
-
-testMail();
-
-
 // Routes
 app.use("/api/auth", authRoutes);
 
@@ -65,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
 });
+
 
 
 
