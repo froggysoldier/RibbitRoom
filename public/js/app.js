637afeee2ced304708ac360bd0f7b7932bcc0914
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 11:01:41 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,39 @@
 // public/js/app.js
 import * as DOM from "./domElements.js";
 import { initAuthHandlers } from "./authHandlers.js";
-import { initSocket } from "./socketClient.js";
 import { initChatHandlers, loadMessages } from "./chatHandlers.js";
+import { initSocket } from "./socketClient.js";
 
+// State initialisieren
+const state = {
+  token: localStorage.getItem("token") || null,
+  username: localStorage.getItem("username") || null,
+  myRole: "user",
+  filterActive: false,
+  socket: null,
+  socketConnected: false,
+  chatWindow: null,
+  sendBtn: null,
+  messageInput: null,
+  filterBtn: null
+};
+
+// --- Alles erst nach DOMContentLoaded ---
 document.addEventListener("DOMContentLoaded", () => {
-  // DOM-Elemente setzen
+  // DOM-Elemente initialisieren
   DOM.initDomElements();
 
-  // State initialisieren
-  const state = {
-    token: localStorage.getItem("token") || null,
-    username: localStorage.getItem("username") || null,
-    myRole: "user",
-    socket: null,
-    socketConnected: false,
-    filterActive: false
-  };
+  // State mit DOM-Elementen verbinden
+  state.chatWindow = DOM.chatWindow;
+  state.sendBtn = DOM.sendBtn;
+  state.messageInput = DOM.messageInput;
+  state.filterBtn = DOM.filterBtn;
 
   // Socket, Auth & Chat initialisieren
   initSocket(state);
   initAuthHandlers(state);
   initChatHandlers(state);
 
-  // Alte Nachrichten laden, falls Token vorhanden
+  // Alte Nachrichten laden
   if (state.token) loadMessages(state);
 });
