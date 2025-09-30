bdfb9f7fbb459d7def8045556a992ba38ab7ed02
justamonkey1 <daniel.knauss07@gmail.com>
2025-09-30 09:57:54 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+// models/User.js
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -21,4 +22,4 @@ UserSchema.methods.comparePassword = async function(candidate) {
   return bcrypt.compare(candidate, this.password);
 };
 
-module.exports = mongoose.model("User", UserSchema);
+export default mongoose.model("User", UserSchema);
