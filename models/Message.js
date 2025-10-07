3a0be2cbc52430d1e742b16ff7d514118fd0ba29
justamonkey1 <daniel.knauss07@gmail.com>
2025-10-07 10:56:57 +0200

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
   sender: String,
@@ -7,4 +7,5 @@ const messageSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
