25f06e7ceb9d436a6264c0daac404d3688b053dc
justamonkey1 <daniel.knauss07@gmail.com>
2025-10-14 15:26:53 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,21 @@
-// models/User.js
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" },
-
-  verificationCode: String,
-  codeExpiresAt: Date
+  role: { type: String, default: "user" } // user oder admin
 });
 
-  UserSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  });
-
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 UserSchema.methods.comparePassword = async function(candidate) {
   return bcrypt.compare(candidate, this.password);
 };
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
