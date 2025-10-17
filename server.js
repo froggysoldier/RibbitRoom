c166362493a302b73e37889849fd66b30253fe6f
froggysoldier <froggysoldier@gmail.com>
2025-10-17 19:29:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Statische Dateien immer frisch laden
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB
@@ -31,3 +38,4 @@ initSockets(io);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => console.log(`✅ Server läuft auf Port ${PORT}`));
+
