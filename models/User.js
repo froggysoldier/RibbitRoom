5be7d4af6ad14c65e904a5ded7cdcc74f3cb1963
justamonkey1 <daniel.knauss07@gmail.com>
2025-10-21 15:24:52 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, default: "user" }, // user oder admin
-  fishProgress: { type: FishProgressSchema, default: () => ({}) }, // neu hinzugefügt
+  fishProgress: { type: FishProgressSchema, default: () => ({}) }, // Fortschritt
   createdAt: { type: Date, default: Date.now }
 });
 
